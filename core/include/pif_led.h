4f691ef616148bd58cc76e9f7496f6390d9efce6
跣辿 <joon0323@naver.com>
2022-01-12 21:18:42 +0900

    1. PifLed 기능 개선

--- core/include/pif_led.h
+++ core/include/pif_led.h
@@ -8,6 +8,18 @@
 typedef void (*PifActLedState)(PifId id, uint32_t state);
 
 /**
+ * @class StPifLedBlink
+ * @brief
+ */
+typedef struct StPifLedBlink
+{
+	SWITCH state;
+	uint16_t multiple;
+	uint16_t count;
+	uint32_t flag;
+} PifLedBlink;
+
+/**
  * @class StPifLed
  * @brief
  */
@@ -22,8 +34,8 @@ typedef struct StPifLed
 	// Private Member Variable
 	PifTimerManager* __p_timer_manager;
 	uint32_t __state;
-	SWITCH __blink;
-	uint32_t __blink_flag;
+	uint8_t __blink_count;
+	PifLedBlink* __p_blinks;
 	PifTimer* __p_timer_blink;
 
 	// Private Action Function
@@ -55,37 +67,37 @@ BOOL pifLed_Init(PifLed* p_owner, PifId id, PifTimerManager* p_timer_manager, ui
 void pifLed_Clear(PifLed* p_owner);
 
 /**
- * @fn pifLed_EachOn
+ * @fn pifLed_PartOn
  * @brief
  * @param p_owner
- * @param index
+ * @param bits
  */
-void pifLed_EachOn(PifLed* p_owner, uint8_t index);
+void pifLed_PartOn(PifLed* p_owner, uint8_t bits);
 
 /**
- * @fn pifLed_EachOff
+ * @fn pifLed_PartOff
  * @brief
  * @param p_owner
- * @param index
+ * @param bits
  */
-void pifLed_EachOff(PifLed* p_owner, uint8_t index);
+void pifLed_PartOff(PifLed* p_owner, uint8_t bits);
 
 /**
- * @fn pifLed_EachChange
+ * @fn pifLed_PartChange
  * @brief
  * @param p_owner
- * @param index
+ * @param bits
  * @param state
  */
-void pifLed_EachChange(PifLed* p_owner, uint8_t index, SWITCH state);
+void pifLed_PartChange(PifLed* p_owner, uint8_t bits, SWITCH state);
 
 /**
- * @fn pifLed_EachToggle
+ * @fn pifLed_PartToggle
  * @brief
  * @param p_owner
- * @param index
+ * @param bits
  */
-void pifLed_EachToggle(PifLed* p_owner, uint8_t index);
+void pifLed_PartToggle(PifLed* p_owner, uint8_t bits);
 
 /**
  * @fn pifLed_AllOn
@@ -117,13 +129,24 @@ void pifLed_AllChange(PifLed* p_owner, uint32_t state);
 void pifLed_AllToggle(PifLed* p_owner);
 
 /**
- * @fn pifLed_AttachBlink
+ * @fn pifLed_AttachSBlink
  * @brief
  * @param p_owner
  * @param period1ms
  * @return
  */
-BOOL pifLed_AttachBlink(PifLed* p_owner, uint16_t period1ms);
+BOOL pifLed_AttachSBlink(PifLed* p_owner, uint16_t period1ms);
+
+/**
+ * @fn pifLed_AttachMBlink
+ * @brief
+ * @param p_owner
+ * @param period1ms
+ * @param count
+ * @param ...
+ * @return
+ */
+BOOL pifLed_AttachMBlink(PifLed* p_owner, uint16_t period1ms, uint8_t count, ...);
 
 /**
  * @fn pifLed_DetachBlink
@@ -142,20 +165,40 @@ void pifLed_DetachBlink(PifLed* p_owner);
 BOOL pifLed_ChangeBlinkPeriod(PifLed* p_owner, uint16_t period1ms);
 
 /**
- * @fn pifLed_BlinkOn
+ * @fn pifLed_SBlinkOn
  * @brief
  * @param p_owner
- * @param index
+ * @param bits
  */
-void pifLed_BlinkOn(PifLed* p_owner, uint8_t index);
+void pifLed_SBlinkOn(PifLed* p_owner, uint8_t bits);
 
 /**
- * @fn pifLed_BlinkOff
+ * @fn pifLed_MBlinkOn
  * @brief
  * @param p_owner
+ * @param bits
  * @param index
  */
-void pifLed_BlinkOff(PifLed* p_owner, uint8_t index);
+void pifLed_MBlinkOn(PifLed* p_owner, uint8_t bits, uint8_t index);
+
+/**
+ * @fn pifLed_SBlinkOff
+ * @brief
+ * @param p_owner
+ * @param bits
+ * @param state
+ */
+void pifLed_SBlinkOff(PifLed* p_owner, uint8_t bits, SWITCH state);
+
+/**
+ * @fn pifLed_MBlinkOff
+ * @brief
+ * @param p_owner
+ * @param bits
+ * @param index
+ * @param state
+ */
+void pifLed_MBlinkOff(PifLed* p_owner, uint8_t bits, uint8_t index, SWITCH state);
 
 #ifdef __cplusplus
 }
--- core/source/pif_led.c
+++ core/source/pif_led.c
@@ -1,24 +1,54 @@
 #include "pif_led.h"
 
 
-static void _evtTimerBlinkFinish(void* p_issuer)
+static void _evtTimerSBlinkFinish(void* p_issuer)
 {
-	BOOL bBlink = FALSE;
-
     PifLed* p_owner = (PifLed*)p_issuer;
-    p_owner->__blink ^= 1;
-    for (uint8_t i = 0; i < p_owner->count; i++) {
-    	if (p_owner->__blink_flag & (1 << i)) {
-    		if (p_owner->__blink) {
-    			p_owner->__state |= 1 << i;
-    		}
-    		else {
-    			p_owner->__state &= ~(1 << i);
-    		}
-    		bBlink = TRUE;
+	PifLedBlink* p_blink = &p_owner->__p_blinks[0];
+	uint8_t i;
+
+	p_blink->state ^= 1;
+	for (i = 0; i < p_owner->count; i++) {
+		if (p_blink->flag & (1UL << i)) {
+			if (p_blink->state) {
+				p_owner->__state |= 1UL << i;
+			}
+			else {
+				p_owner->__state &= ~(1UL << i);
+			}
+		}
+	}
+	(*p_owner->__act_state)(p_owner->_id, p_owner->__state);
+}
+
+static void _evtTimerMBlinkFinish(void* p_issuer)
+{
+    PifLed* p_owner = (PifLed*)p_issuer;
+	PifLedBlink* p_blink;
+	uint8_t b, i;
+
+    for (b = 0; b < p_owner->__blink_count; b++) {
+    	p_blink = &p_owner->__p_blinks[b];
+    	if (!p_blink->count) {
+    		p_blink->count = p_blink->multiple - 1;
+
+    		p_blink->state ^= 1;
+			for (i = 0; i < p_owner->count; i++) {
+				if (p_blink->flag & (1UL << i)) {
+					if (p_blink->state) {
+						p_owner->__state |= 1UL << i;
+					}
+					else {
+						p_owner->__state &= ~(1UL << i);
+					}
+				}
+			}
+    	}
+    	else {
+    		p_blink->count--;
     	}
     }
-    if (bBlink) (*p_owner->__act_state)(p_owner->_id, p_owner->__state);
+	(*p_owner->__act_state)(p_owner->_id, p_owner->__state);
 }
 
 BOOL pifLed_Init(PifLed* p_owner, PifId id, PifTimerManager* p_timer_manager, uint8_t count, PifActLedState act_state)
@@ -46,32 +76,32 @@ void pifLed_Clear(PifLed* p_owner)
 	}
 }
 
-void pifLed_EachOn(PifLed* p_owner, uint8_t index)
+void pifLed_PartOn(PifLed* p_owner, uint8_t bits)
 {
-	p_owner->__state |= 1 << index;
+	p_owner->__state |= bits;
 	(*p_owner->__act_state)(p_owner->_id, p_owner->__state);
 }
 
-void pifLed_EachOff(PifLed* p_owner, uint8_t index)
+void pifLed_PartOff(PifLed* p_owner, uint8_t bits)
 {
-	p_owner->__state &= ~(1 << index);
+	p_owner->__state &= ~bits;
 	(*p_owner->__act_state)(p_owner->_id, p_owner->__state);
 }
 
-void pifLed_EachChange(PifLed* p_owner, uint8_t index, SWITCH state)
+void pifLed_PartChange(PifLed* p_owner, uint8_t bits, SWITCH state)
 {
 	if (state) {
-		p_owner->__state |= 1 << index;
+		p_owner->__state |= bits;
 	}
 	else {
-		p_owner->__state &= ~(1 << index);
+		p_owner->__state &= ~bits;
 	}
 	(*p_owner->__act_state)(p_owner->_id, p_owner->__state);
 }
 
-void pifLed_EachToggle(PifLed* p_owner, uint8_t index)
+void pifLed_PartToggle(PifLed* p_owner, uint8_t bits)
 {
-	p_owner->__state ^= 1 << index;
+	p_owner->__state ^= bits;
 	(*p_owner->__act_state)(p_owner->_id, p_owner->__state);
 }
 
@@ -99,9 +129,17 @@ void pifLed_AllToggle(PifLed* p_owner)
 	(*p_owner->__act_state)(p_owner->_id, p_owner->__state);
 }
 
-BOOL pifLed_AttachBlink(PifLed* p_owner, uint16_t period1ms)
+BOOL pifLed_AttachSBlink(PifLed* p_owner, uint16_t period1ms)
 {
-	if (!period1ms) {
+	return pifLed_AttachMBlink(p_owner, period1ms, 1, 1);
+}
+
+BOOL pifLed_AttachMBlink(PifLed* p_owner, uint16_t period1ms, uint8_t count, ...)
+{
+	va_list ap;
+	int b;
+
+	if (!period1ms || !count) {
         pif_error = E_INVALID_PARAM;
 		return FALSE;
     }
@@ -109,16 +147,49 @@ BOOL pifLed_AttachBlink(PifLed* p_owner, uint16_t period1ms)
 	if (!p_owner->__p_timer_blink) {
 		p_owner->__p_timer_blink = pifTimerManager_Add(p_owner->__p_timer_manager, TT_REPEAT);
 		if (!p_owner->__p_timer_blink) return FALSE;
-		pifTimer_AttachEvtFinish(p_owner->__p_timer_blink, _evtTimerBlinkFinish, p_owner);
 	}
 
-	p_owner->__blink_flag = 0L;
     pifTimer_Start(p_owner->__p_timer_blink, period1ms * 1000L / p_owner->__p_timer_manager->_period1us);
+
+    p_owner->__p_blinks = calloc(count, sizeof(PifLedBlink));
+    if (!p_owner->__p_blinks) {
+    	pif_error = E_OUT_OF_HEAP;
+    	return FALSE;
+    }
+    p_owner->__blink_count = count;
+
+    if (count == 1) {
+        pifTimer_AttachEvtFinish(p_owner->__p_timer_blink, _evtTimerSBlinkFinish, p_owner);
+
+   		p_owner->__p_blinks[0].multiple = 1;
+    }
+    else {
+        pifTimer_AttachEvtFinish(p_owner->__p_timer_blink, _evtTimerMBlinkFinish, p_owner);
+
+    	va_start(ap, count);
+    	for (b = 0; b < count; b++) {
+    		p_owner->__p_blinks[b].multiple = va_arg(ap, int);
+    		if (!p_owner->__p_blinks[b].multiple) {
+    			free(p_owner->__p_blinks);
+    			p_owner->__p_blinks = NULL;
+    			p_owner->__blink_count = 0;
+    	        pif_error = E_INVALID_PARAM;
+    			return FALSE;
+    		}
+    	}
+    	va_end(ap);
+    }
 	return TRUE;
 }
 
 void pifLed_DetachBlink(PifLed* p_owner)
 {
+    if (p_owner->__p_blinks) {
+		free(p_owner->__p_blinks);
+		p_owner->__p_blinks = NULL;
+    }
+	p_owner->__blink_count = 0;
+
 	if (p_owner->__p_timer_blink) {
 		pifTimerManager_Remove(p_owner->__p_timer_blink);
 		p_owner->__p_timer_blink = NULL;
@@ -141,13 +212,31 @@ BOOL pifLed_ChangeBlinkPeriod(PifLed* p_owner, uint16_t period1ms)
 	return TRUE;
 }
 
-void pifLed_BlinkOn(PifLed* p_owner, uint8_t index)
+void pifLed_SBlinkOn(PifLed* p_owner, uint8_t bits)
 {
-    p_owner->__blink_flag |= 1 << index;
+    p_owner->__p_blinks[0].flag |= bits;
 }
 
-void pifLed_BlinkOff(PifLed* p_owner, uint8_t index)
+void pifLed_MBlinkOn(PifLed* p_owner, uint8_t bits, uint8_t index)
 {
-	p_owner->__blink_flag &= ~(1 << index);
-	pifLed_EachOff(p_owner, index);
+	uint8_t b;
+
+    p_owner->__p_blinks[index].flag |= bits;
+    for (b = 0; b < p_owner->__blink_count; b++) {
+    	if (b != index && (p_owner->__p_blinks[b].flag & bits)) {
+    		p_owner->__p_blinks[b].flag &= ~bits;
+    	}
+    }
+}
+
+void pifLed_SBlinkOff(PifLed* p_owner, uint8_t bits, SWITCH state)
+{
+	p_owner->__p_blinks[0].flag &= ~bits;
+	pifLed_PartChange(p_owner, bits, state);
+}
+
+void pifLed_MBlinkOff(PifLed* p_owner, uint8_t bits, uint8_t index, SWITCH state)
+{
+	p_owner->__p_blinks[index].flag &= ~bits;
+	pifLed_PartChange(p_owner, bits, state);
 }
